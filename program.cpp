2a9cbe26c6a9e7340658861d157b24eaef44b377
TsoyIgor <igor1907tsoy@gmail.com>
2024-04-10 23:23:24 +0400

    added function fib(n)

--- program.cpp
+++ program.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+    if (n == 2 || n == 3)
+        return (n - 1);
+    return fib(n - 1) + fib(n - 2);
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    cout << fib(10) << endl;
     return 0;
 }
